d95bc215b6a0bcc2f0c08aa3bed0727e00518127
Edilson <developeredilson@gmail.com>
2022-05-19 12:17:55 +0200

    fished message input

--- front/src/chat/pages/chat/components/msg-input/MsgInput.tsx
+++ front/src/chat/pages/chat/components/msg-input/MsgInput.tsx
@@ -1,21 +1,40 @@
 import "./msgInputStyle.css";
 import type { FC, ReactElement } from "react";
-import { EmojiSmile , CameraFill, MicFill} from "react-bootstrap-icons";
+import { useState, useRef } from "react";
+import { EmojiSmile , CameraFill, MicFill, SendFill } from "react-bootstrap-icons";
 
 const MsgInput:FC = ():ReactElement => {
+
+  const [sendTextMessage, setSendTextMessage] = useState(false);
+  const messageInput = useRef<HTMLTextAreaElement>(null);
+  
+  const handleOnInputMessage = () => {
+    if (messageInput.current!.value.length > 0)
+      setSendTextMessage(true)
+    else
+      setSendTextMessage(false)
+  }
+
   return (
     <div className="msg-input">
+      <div> 
+        
+        <div className="msg-text">
+          <EmojiSmile className="msg-icon smile" />
+          <textarea onInput={ handleOnInputMessage } ref={messageInput} placeholder="write some text" className="msg-text-input" rows={1} ></textarea>
+          <CameraFill className="msg-icon camera" />
+        </div>
+        
+        <div className="msg-audio">
+          {
+            sendTextMessage ? 
+              <SendFill className="msg-icon mic" /> 
+            : 
+              <MicFill className="msg-icon mic" /> 
+          }
+        </div>
       
-      <div className="msg-text">
-        <EmojiSmile className="msg-icon smile" />
-        <input placeholder="write some text" className="msg-text-input" />
-        <CameraFill className="msg-icon camera" />
       </div>
-      
-      <div className="msg-audio">
-        <MicFill className="msg-icon mic" /> 
-      </div>
-
     </div>
   )
 }
